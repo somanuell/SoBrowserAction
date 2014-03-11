01bc36d95c070ea17161c9f0697477131f85b3ce
somanuell <manuel.leclerc@orange.fr>
2014-03-11 18:53:43 +0100

    clean-up

--- SoBrowserActionBHO/SoBrowserActionBHO_i.h
+++ SoBrowserActionBHO/SoBrowserActionBHO_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Mar 11 17:11:35 2014
+/* at Tue Mar 11 18:45:08 2014
  */
 /* Compiler settings for SoBrowserActionBHO.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
